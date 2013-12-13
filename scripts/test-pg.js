0b842626d9a2f29da2f9ff0bca961f0c717ffbe3
Colin Rennie <colinmatthew@gmail.com>
2013-12-12 17:13:23 -0800

    remove scripts

--- scripts/test-pg.js
+++
@@ -1,9 +0,0 @@
-
-var pg = require('pg');
-
-var conString = "tcp://hosting-db:alphak1ll$@localhost/postgres"; // REMEMBER 'NFL' SCHEMA PREFIX IN QUERIES!
-
-var client = new pg.Client(conString);
-client.connect();
-
-
